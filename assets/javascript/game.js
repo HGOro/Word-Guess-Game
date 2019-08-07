9b7b4bddd394f950c5ac76b9cd0ad05172da1e0a
HG_ORO <h.g.orozco@gmail.com>
2019-08-06 21:54:23 -0700

    mostly js pseudo, minor changes to html

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+    
+
+
+
+
+
+
+
+})
+    
+//Player INPUT. Will need to have an onkey event that connects to above correct/incorrect displays
+//reveal player guesses (letters input) not in the word
+
+
+// if letter guessed is correct, reveal the letter in the word box
+// if Letter guessed incorrectly, display in incorrect guesses box
+// if word is completed before guesses run out, add 1 to Win count, restart with new word
+// if guess count reaches zero, add 1 to Loss count, restart with new word
+
+
+
+//First, need to display the number of letters in word as "_"'s
+
+//Guesses remaining.
+//Counter for number of guesses
+//reset once counter reaches zero
+
+//Win Count, shows how many words have been guessed correctly
+//Loss Count, shows how many words have NOT been guessed
+
+//Game reset
+//when word is completely guessed
+// or IF word is not guessed in time
+
+
+
+
+//~~~~~~~~~words to use ~~~~~~~~~~~~
+// why
+
+// hour
+// heir
+// what
+// when
+// hola
+
+// ghost
+// ghoul
+// rhyme
+// honor
+
+// honest
+// chorus
+// rhythm
+// light
+
+// rhubarb
+// through
+
+
+// exhausted
+
+
